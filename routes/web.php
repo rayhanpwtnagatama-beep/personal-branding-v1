bd7f079f58a892222d315d19742e7a7169920c1b
Rayhan Pawitra Nagatama <rayhanpwtnagatama@gmail.com>
2026-04-06 00:30:45 +0700

    ha

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [PortfolioController::class, 'index']);
+
+Route::get('/setup-database', function() {
+    Artisan::call('db:seed', ['--force' => true]);
+    return 'Database successfully seeded! You can now go back to the homepage.';
+});
